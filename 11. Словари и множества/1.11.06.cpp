53951205792277121beee0f4e0ff55cdf26decbd
ToriikiA <i.fedorov@reglab.ru>
2025-07-24 12:16:36 +0500

    Добавление задач

--- 11. Словари и множества/1.11.06.cpp
+++ 11. Словари и множества/1.11.06.cpp
@@ -1,1 +1,44 @@
 // I'll be back...
+//
+// Я вернулся.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+// Комментарий оставлю потом
+
+int main() {
+  int N;
+    cin >> N;
+    map<string, vector<string>> A;
+    for(int i = 0; i < N; ++i){
+        string latw, engw;
+        cin >> engw >> latw;
+        bool end = false;
+        while(!end){
+            cin >> latw;
+            int last_index = latw.length() - 1;
+            if(latw[last_index] == ','){
+                latw = latw.erase(last_index);
+            }
+            else{
+                end = true;
+            }
+            A[latw].push_back(engw);
+        }
+    }
+    cout << A.size() << "\n";
+    for(auto x : A){
+        cout << x.first << " - ";
+        for(int i = 0; i < x.second.size() - 1; ++i){
+            cout << x.second[i] << ", ";
+        }
+        cout << x.second[x.second.size() - 1] << "\n";
+    }
+  return 0;
+}
--- 11. Словари и множества/1.11.07.cpp
+++ 11. Словари и множества/1.11.07.cpp
@@ -1,1 +1,79 @@
 // I'll be back...
+//
+// Я вернулся
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <locale>
+#include <ctype.h>
+
+using namespace std;
+
+// Комментарии допишу потом...
+// В рамках решения этой задачи были проведены изыскания по работе со строками. Поверхностно изучены библиотеки locale и ctype.h
+//
+//
+
+void split(string &in_str, char symb, vector<string> &out_str){
+    int start, end = 0;
+    while((start = in_str.find_first_not_of(symb, end)) != string::npos){
+        end = in_str.find(symb, start);
+        out_str.push_back(in_str.substr(start, end-start));
+    }
+}
+
+int main() {
+    int N, errors = 0;
+    cin >> N >> ws;
+    set<string> dict, low_dict;
+    for(int i = 0; i < N;++i){
+        string word;
+        word.clear();
+        cin >> word >> ws;
+        dict.insert(word);
+        locale loc;
+        string low_word = "";
+        for(auto x : word){
+            low_word += tolower(x, loc);
+        }
+        low_dict.insert(low_word);
+    }
+    
+    string hwork;
+    getline(cin, hwork);
+    if(hwork.empty()){
+        cout << 0;
+    }
+    else{
+        char space = ' ';
+        vector<string> splitted_hwork;
+        split(hwork, space, splitted_hwork);
+        for(auto x : splitted_hwork){
+            int caps = 0;
+            for(int i = 0; i < x.size(); ++i){
+                if(isupper(x[i])){
+                    ++caps;
+                }
+            }
+            if(caps != 1){
+                ++errors;
+            }
+            else{
+                if(dict.find(x) == dict.end() && caps == 1){
+                    locale loc1;
+                    string f_low_word = "";
+                    for(auto y : x){
+                        f_low_word += tolower(y, loc1);
+                    }
+                    if(low_dict.find(f_low_word) != low_dict.end()){
+                        ++errors;
+                    }
+                }
+            }
+        }
+        cout << errors;
+    }
+  return 0;
+}
